6b822f41086f956d23942c359ba6133382981b5a
wupg98 <106410199+wupg98@users.noreply.github.com>
2022-07-20 14:00:16 +0200

    Update Modifikation.java

--- java-forum/Modifikation.java
+++ java-forum/Modifikation.java
@@ -1,1 +1,50 @@
 //
+protected static int max(int player, int depth){
+
+    if(depth == 0){
+
+      quantity += 1;
+
+      System.out.println(quantity);
+
+      return 1;
+
+    }
+
+    int maxValue = Integer.MIN_VALUE;
+
+    int[] possibleMoves = generateMoves();
+
+    while(depth > 0) {
+
+      for (int no = 0; no < possibleMoves.length; no++) {
+
+        moveForward(player, possibleMoves[no]);
+
+        ++Game.round;
+
+        int value = min(-player, depth - 1);
+
+        moveBack(possibleMoves[no]);
+
+        --Game.round;
+
+        if (value > maxValue) {
+
+          maxValue = value;
+
+          if (depth == 1) {
+
+            savedMove = move;
+
+          }
+
+        }
+
+      }
+
+    }
+
+    return maxValue;
+
+  }
