b9834eb4511896843ef5a8ff27c1f7221dcd475f
wupg98 <106410199+wupg98@users.noreply.github.com>
2022-07-20 14:12:24 +0200

    Update Modifikation.java

--- java-forum/Modifikation.java
+++ java-forum/Modifikation.java
@@ -48,3 +48,29 @@ protected static int max(int player, int depth){
     return maxValue;
 
   }
+
+
+private static int[] generateMoves(){
+
+    int[] free = new int[(64 - (Game.round))];
+
+    int j = 0; //1 in R
+
+    for(int i = 0; i < 64; i++) {  //1 in R  <=64
+
+      if(Game.game[i] == 0) { //Game.game[I] == "NULL" 
+
+        free[j] = i;
+
+        j++;
+
+      }
+
+    }
+ //Methode muss wahrscheinlich umgeschrieben werden, da nur relevant ist WELCHE FELDER (aber kumulativ nicht bloß diesen Zug) FREI WÄREN...
+    return free;
+
+  }
+//private static void moveBack(int fieldno){
+    //0=Standardwert für Felder  (moveBack nur)
+    // (Zweite Modifikation)
